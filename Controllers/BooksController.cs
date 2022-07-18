8bb48fc4341c5c79929e0694c5fd5c7a9208ec98
Azamat <az1k3@outlook.com>
2022-07-18 17:10:55 +0500

    Added CRUD actions for WEB API

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -15,11 +15,41 @@ namespace my_books.Controllers
             _booksService = booksService;
         }
 
+        [HttpGet("get-all-books")] 
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        [HttpGet("get-book-by-id/{id}")]
+        public IActionResult GetBookById(int id)
+        {
+            var book = _booksService.GetBookById(id);
+            return Ok(book);
+        }
+
         [HttpPost("add-book")]
         public IActionResult AddBook([FromBody] BookVM book)
         {
             _booksService.AddBook(book);
             return Ok();
         }
+
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();
+        }
+
+
     }
 }
